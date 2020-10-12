fb29cd0d71bf7fbe191e9ac15ebc2fe6f75c5d89
Administrator <Administrator@MLCTraining.local>
2020-10-12 14:12:09 +0200

    Projektdateien hinzufügen.

---
+++ SPNewsticker/Features/Feature1/Feature1.feature
@@ -0,0 +1,9 @@
+﻿<?xml version="1.0" encoding="utf-8"?>
+<feature xmlns:dm0="http://schemas.microsoft.com/VisualStudio/2008/DslTools/Core" dslVersion="1.0.0.0" Id="9c761bba-ecdf-450c-9eb9-9be479bd7383" description="SharePoint-Add-In-Feature" featureId="9c761bba-ecdf-450c-9eb9-9be479bd7383" imageUrl="" solutionId="00000000-0000-0000-0000-000000000000" title="SPNewsticker Feature1" version="" deploymentPath="$SharePoint.Project.FileNameWithoutExtension$_$SharePoint.Feature.FileNameWithoutExtension$" xmlns="http://schemas.microsoft.com/VisualStudio/2008/SharePointTools/FeatureModel">
+  <projectItems>
+    <projectItemReference itemId="ff6f4a6a-fd34-4bcd-8374-ecf7cb5fda8a" />
+    <projectItemReference itemId="0d30810a-64f9-42f1-ad16-b1a151713134" />
+    <projectItemReference itemId="f594ca86-cdd6-40ba-94ae-b0d6c8d4ecb4" />
+    <projectItemReference itemId="0f3b84c9-ddf0-4760-9f68-421d7c99a96a" />
+  </projectItems>
+</feature>
---
+++ SPNewsticker/Scripts/App.js
@@ -0,0 +1,84 @@
+﻿     'use strict';
+
+var hostUrl = '';
+var allNews;
+var context = SP.ClientContext.get_current();
+
+// Dieser Code wird ausgeführt, wenn das DOM bereit ist. Es wird ein Kontextobjekt erstellt, das zur Verwendung des SharePoint-Objektmodells erforderlich ist.
+$(document).ready(function () {
+    //Host Web Context erstellen
+    var hostweburl = decodeURIComponent(getQueryStringParameter("SPHostUrl"));
+    var hostcontext = new SP.AppContextSite(context, hostweburl);
+    var web = hostcontext.get_web();
+
+    //laden der Listenelemente aus der Liste News des Host Webs
+    var list = web.get_lists().getByTitle("News");
+    var camlString =
+        "<View><ViewFields>" +
+        "<FieldRef Name='Title' />" +
+        "<FieldRef Name='Nachricht' />" +
+        "</ViewFields></View>";
+
+    var camlQuery = new SP.CamlQuery();
+    camlQuery.set_viewXml(camlString);
+    allNews = list.getItems(camlQuery);
+
+    context.load(allNews, "Include(Title, Nachricht)");
+    context.executeQueryAsync(onQuerySucceeded, onQueryFailed);
+});
+
+function onQuerySucceeded() {
+    var newsHTML = "";
+    var enumerator = allNews.getEnumerator();
+
+    //Aufbau des HTML-Codes mit li-Tags
+    while (enumerator.moveNext()) {
+        var announcement = enumerator.get_current();
+        newsHTML = newsHTML +
+            "<li><span>  +++  " + announcement.get_item("Title") + ": </span>" + announcement.get_item("Nachricht") + " </li>";
+            }
+
+    $("#ticker01")[0].innerHTML = newsHTML;
+
+    //Initialisierung des ul-Tags mit dem liScroll - Befehl
+    $("ul#ticker01").liScroll({ travelocity: "0.15" });
+};
+
+function onQueryFailed(sender, args) {
+    alert('Fehler: ' + args.get_message() +
+        '\n' + args.get_stackTrace());
+};
+
+function getQueryStringParameter(paramToRetrieve) {
+    var params;
+    var strParams;
+
+    params = document.URL.split("?")[1].split("&");
+    strParams = "";
+    for (var i = 0; i < params.length; i = i + 1) {
+        var singleParam = params[i].split("=");
+        if (singleParam[0] == paramToRetrieve)
+            return singleParam[1];
+    }
+}
+
+function onQueryFailed(sender, args) {
+    alert('Fehler: ' + args.get_message() +
+        '\n' + args.get_stackTrace());
+}
+
+function getQueryStringParameter(paramToRetrieve) {
+    var params
+    var strParams;
+
+    params = document.URL.split("?")[1].split("&");
+    strParams = "";
+    for (var i = 0; i < params.length; i = i + 1) {
+        var singleParam = params[i].split("=");
+        if (singleParam[0] == paramToRetrieve)
+            return singleParam[1];
+    }
+}
+
+        
+
